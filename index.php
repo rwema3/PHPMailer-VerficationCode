3605291d6ced6b9bacd345e566e25796ed75dba3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-01 12:41:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
 </head>
 
